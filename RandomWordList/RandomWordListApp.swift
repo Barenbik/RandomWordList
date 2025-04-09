493064e34d42f242d4e4a5126e9365ca680e7b5f
Tony Sharples <tonysharples22@me.com>
2025-04-09 12:38:30 +0100

    Initial Commit

---
+++ RandomWordList/RandomWordListApp.swift
@@ -0,0 +1,17 @@
+//
+//  RandomWordListApp.swift
+//  RandomWordList
+//
+//  Created by Tony Sharples on 09/04/2025.
+//
+
+import SwiftUI
+
+@main
+struct RandomWordListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
